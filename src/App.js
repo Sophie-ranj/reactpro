b3b8d2a396aa4fb02f6459e4640f66af6f75969c
safoora1986 <safoora.ranjbaran@gmail.com>
2023-06-16 21:53:27 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sophie Ranjbaran and is{" "}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/safoora1986/reactpro"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <button className="btn btn-success">Hello</button>
+        <footer>
+          This project was coded by Sophie Ranjbaran and is{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/safoora1986/reactpro"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

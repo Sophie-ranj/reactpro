0bac0df4efcfb7fe8e5a43144189f118e7bef0b0
safoora1986 <safoora.ranjbaran@gmail.com>
2023-06-16 19:32:32 +0100

    editing some errors

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Sophie Ranj and is
-        <a target="_blank" src="https://github.com/safoora1986/reactpro">
+        This project was coded by Sophie Ranj and is{" "}
+        <a target="_blank" href="https://github.com/safoora1986/reactpro">
           open-sourced on GitHub
         </a>
       </footer>

4b276bba0302d7089d4cdc40bce69f943faa9a3a
safoora1986 <safoora.ranjbaran@gmail.com>
2023-06-16 21:33:00 +0100

    add rel to a tag

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Sophie Ranjbaran and is{" "}
-        <a target="_blank" href="https://github.com/safoora1986/reactpro">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/safoora1986/reactpro"
+        >
           open-sourced on GitHub
         </a>
       </footer>
